779646618e77448c216524cc691f87057d8f7b94
joshua-mcfield99 <72096757+joshua-mcfield99@users.noreply.github.com>
2024-08-21 16:20:59 +0100

    Endpoint plan & Database connection

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Imports
+require('dotenv').config();
 const express = require('express');
-
+const pool = require('./database_sql/pool');
 
 //Intialize app
 const app = express();
@@ -11,6 +12,24 @@ const PORT = process.env.PORT || 3000;
 //Test basic route
 app.get('/', (req, res) => {
     res.send('Hello World');
+});
+
+// Test route to fetch data from the database
+app.get('/users', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT * FROM users');
+        res.json(result.rows);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+// Shutdown the pool on SIGTERM signal
+process.on('SIGTERM', () => {
+    pool.end(() => {
+        console.log('Pool has ended');
+    });
 });
 
 //Listen
